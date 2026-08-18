97739ad30c0c2b1480f7f99253859caebfb2453f
TURAG10 <taslima2092965@gmail.com>
2026-08-19 00:52:15 +0600

    4th

---
+++ functions/arrow.js
@@ -0,0 +1,9 @@
+"use strict";
+// const greet = (name: string): void => {
+//     console.log(`Hello ${name}`);
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+// greet("Aatish");
+const square = (num) => num * num;
+console.log(square(5));
+//# sourceMappingURL=arrow.js.map
---
+++ functions/arrow.ts
@@ -0,0 +1,9 @@
+// const greet = (name: string): void => {
+//     console.log(`Hello ${name}`);
+// };
+
+// greet("Aatish");
+
+const square = (num: number): number => num * num;
+
+console.log(square(10));
