df77fb99f12e3b0f6a5cca86c559924d399e6f81
TURAG10 <taslima2092965@gmail.com>
2026-08-23 00:53:00 +0600

    fil

---
+++ arrays/filter.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers);
+//# sourceMappingURL=filter.js.map
