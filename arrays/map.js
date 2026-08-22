b635006fd8ab17712007f48db9e0738d2f328853
TURAG10 <taslima2092965@gmail.com>
2026-08-22 21:12:41 +0600

    2nd

---
+++ arrays/map.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
+//# sourceMappingURL=map.js.map
